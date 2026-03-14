216119cdff09967fc25158bdaca4afb16b3e9536
Indra Zulfi <me@indrazm.com>
2026-03-14 12:52:53 +0700

    init monorepo

--- apps/platform/src/utils/api.ts
+++ apps/platform/src/utils/api.ts
@@ -1,7 +1,7 @@
 import { hc } from "hono/client";
 import type { AppType } from "../../../api/src/index";
 
-const API_URL = process.env.API_URL || "http://localhost:8000";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 console.log({ API_URL });
 
 export const api = hc<AppType>(API_URL);
